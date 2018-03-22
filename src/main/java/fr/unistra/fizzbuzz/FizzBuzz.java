1f0bee65449ca5112fe4746400bf732d437d71ad
Pauline Capesius <pauline.capesius@etu.unistra.fr>
2018-03-22 16:37:43 +0100

    ex1

---
+++ src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+package fr.unistra.fizzbuzz;
+
+public class FizzBuzz {
+
+	public String convert(int number) {
+		String res ="";
+		if(number % 3 == 0 && number%5==0) {
+			res = "FizzBuzz";
+		}
+		else if (number %3==0) {
+			res ="Fizz";
+		}
+		else if (number %5==0) {
+			res ="Buzz";
+		}
+		else {
+			res = number+"";
+		}
+		return res;
+	}
+}
